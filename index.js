5969c8209f52c6b5fc86fc0386cd5964b66b653f
babkin_nikita <shrud2k@gmail.com>
2019-06-06 18:30:32 +0300

    init

---
+++ index.js
@@ -0,0 +1,50 @@
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+    console.log('${bot.user.username} online');
+    bot.user.setGame("Here to help you!");
+});
+
+bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(cmd === `${prefix}help`){
+        return message.channel.send("Under construction");
+    }
+
+    if(cmd === `${prefix}info`){
+        let botAvatar = bot.user.displayAvatarURL;
+        let botembed = new Discord.RichEmbed()
+        .setDescription("Bot usage Information")
+        .setColor("#d45f93")
+        .setThumbnail(botAvatar)
+        .addField("Hello, my name is "+ bot.user.username + ", I am here to help you, please be aware that I am still learning to do stuff ;)", "CS.MONEY Developer Team");
+        
+        return message.channel.send(botembed);
+    }
+
+    if(message.isMentioned('574390338123333633')) {
+        let botAvatar = bot.user.displayAvatarURL;
+        let embedResponse = new Discord.RichEmbed()
+        .setThumbnail(botAvatar)
+        .setDescription("Q: Where can i find support on the site?")
+        .setColor("#d45f93")
+        .addField("A: To find our site's support please navigate to the bottom right part of the site.\nThere you can get all the help you need 24/7, 365 days a year.", "Yours CS.MONEY Support Team")
+
+        return message.reply({
+            embed: embedResponse,
+            files: ["./support_placement.png"],
+        })
+    }
+});
+
+bot.login(botconfig.token);
