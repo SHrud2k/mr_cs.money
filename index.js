44fbeea3a63d231ef6fafcbf1f56a384e4e9009d
Raim <erzhan.bmstu@gmail.com>
2019-06-06 18:38:38 +0300

    BBB

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
-
+///asdassad
 const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
