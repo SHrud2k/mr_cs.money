0dce64ee164433a929548d204cd2262222fcb2ca
SHrud2k <36342977+SHrud2k@users.noreply.github.com>
2019-06-11 12:22:32 +0300

    Fixed conflicts (#3)
    * Update index.js
    
    * Update index.js
    
    * Added Skins statuscheck
    
    * added skinList for urls
    
    * added notify command and pictures
    
    * Update package.json

--- external_functions.js
+++ external_functions.js
@@ -1,3 +1,9 @@
+const Discord = require("discord.js");
+const got = require("got");
+var fs = require("fs");
+const store = require("nedb");
+const db = new store({ filename: "database.db", autoload: true });
+
 function similarity(s1, s2) {
     var longer = s1;
     var shorter = s2;
@@ -41,4 +47,125 @@ function editDistance(s1, s2) {
     return costs[s2.length];
 }
 
-module.exports = similarity;
+function getImageUrlFromName(skinName) {
+    //Probably better to store in memory
+    var skinList = JSON.parse(fs.readFileSync("skinList.json", "utf8"));
+    return `https://pic.csgo.trade/730/${skinList[skinName]}.jpg?v=22 `;
+}
+
+function getRichEmbed(data, skinName) {
+    if (data.overstock_difference == undefined) {
+        let botAvatar = bot.user.displayAvatarURL;
+
+        let embedResponse = new Discord.RichEmbed()
+            .setThumbnail(getImageUrlFromName(skinName))
+            .setDescription(`Skin Status of ${skinName}`)
+            .setColor("#d45f93")
+            .addField(
+                `Trade status: ${
+                    data.type
+                } \nUse ${prefix}notify ${skinName} to get notified when the skin is tradeable`,
+                `[Get ${skinName} on CS.Money](https://cs.money/?s=float#skin_name_buy=${encodeURIComponent(
+                    skinName
+                )}${skinName.endsWith(")") ? "" : ")"}`
+            );
+        return embedResponse;
+    } else {
+        let botAvatar = bot.user.displayAvatarURL;
+        let embedResponse = new Discord.RichEmbed()
+            .setThumbnail(getImageUrlFromName(skinName))
+            .setDescription(`Skin Status of ${skinName}`)
+            .setColor("#d45f93")
+            .addField(
+                `Trade status: ${data.type} \nAmount till overstock: ${
+                    data.overstock_difference
+                }`,
+                `[Get ${skinName} on CS.Money](https://cs.money/?s=float#skin_name_buy=${encodeURIComponent(
+                    skinName
+                )}${skinName.endsWith(")") ? "" : ")"}`
+            );
+        return embedResponse;
+    }
+}
+
+function checkItemStatus2(docs) {
+    if (docs.length > 0) {
+        currentDoc = docs[0];
+        got(
+            `https://cs.money/check_skin_status?market_hash_name=${
+                currentDoc.skin
+            }&appid=730`,
+            { json: true }
+        )
+            .then(response => {
+                let data = response.body;
+                if (data.type == "Tradable") {
+                    for (var idIndex in currentDoc.ids) {
+                        bot.fetchUser(currentDoc.ids[idIndex]).then(user =>
+                            user.send(getRichEmbed(data, currentDoc.skin))
+                        );
+                    }
+                    db.remove({ _id: currentDoc._id }, {}, function(
+                        err,
+                        numRemoved
+                    ) {});
+                }
+                checkItemStatus2(docs.slice(1));
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+}
+
+function startItemCheck() {
+    db.find({}, function(err, docs) {
+        checkItemStatus2(docs);
+    });
+    setTimeout(function() {
+        startItemCheck();
+    }, 10 * 60 * 1000);
+}
+
+function checkItemStatus() {
+    db.find({}, function(err, docs) {
+        for (var docIndex in docs) {
+            got(
+                `https://cs.money/check_skin_status?market_hash_name=${
+                    docs[docIndex].skin
+                }&appid=730`,
+                { json: true }
+            )
+                .then(response => {
+                    let data = response.body;
+                    if (data.type == "Tradable") {
+                        for (var idIndex in docs[docIndex].ids) {
+                            bot.fetchUser(docs[docIndex].ids[idIndex]).then(
+                                user =>
+                                    user.send(
+                                        getRichEmbed(data, docs[docIndex].skin)
+                                    )
+                            );
+                        }
+                        db.remove({ _id: docs[docIndex]._id }, {}, function(
+                            err,
+                            numRemoved
+                        ) {});
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+    });
+}
+
+module.exports = {
+    similarity,
+    editDistance,
+    getImageUrlFromName,
+    getRichEmbed,
+    checkItemStatus2,
+    startItemCheck,
+    checkItemStatus
+};
