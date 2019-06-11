1e92db1c106098b9c2660ce16a0c6f87ce477cf5
cantryDev <cantrymc@gmail.com>
2019-06-11 15:21:01 +0200

    fixed notify command

---
+++ external_functions.js
@@ -0,0 +1,200 @@
+const Discord = require("discord.js");
+const botconfig = require("./botconfig.json");
+const got = require("got");
+var fs = require("fs");
+const store = require("nedb");
+const db = new store({filename: "database.db", autoload: true});
+
+let bot;
+
+const prefix = botconfig.prefix;
+
+function similarity(s1, s2) {
+    var longer = s1;
+    var shorter = s2;
+    if (s1.length < s2.length) {
+        longer = s2;
+        shorter = s1;
+    }
+    var longerLength = longer.length;
+    if (longerLength == 0) {
+        return 1.0;
+    }
+    return (
+        (longerLength - editDistance(longer, shorter)) /
+        parseFloat(longerLength)
+    );
+}
+
+function editDistance(s1, s2) {
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+
+    var costs = new Array();
+    for (var i = 0; i <= s1.length; i++) {
+        var lastValue = i;
+        for (var j = 0; j <= s2.length; j++) {
+            if (i == 0) costs[j] = j;
+            else {
+                if (j > 0) {
+                    var newValue = costs[j - 1];
+                    if (s1.charAt(i - 1) != s2.charAt(j - 1))
+                        newValue =
+                            Math.min(Math.min(newValue, lastValue), costs[j]) +
+                            1;
+                    costs[j - 1] = lastValue;
+                    lastValue = newValue;
+                }
+            }
+        }
+        if (i > 0) costs[s2.length] = lastValue;
+    }
+    return costs[s2.length];
+}
+
+function getImageUrlFromName(skinName) {
+    //Probably better to store in memory
+    var skinList = JSON.parse(fs.readFileSync("skinList.json", "utf8"));
+    return `https://pic.csgo.trade/730/${skinList[skinName]}.jpg?v=22 `;
+}
+
+function getRichEmbed(data, skinName) {
+    if (data.overstock_difference == undefined) {
+        //let botAvatar = bot.user.displayAvatarURL;
+        let embedResponse = new Discord.RichEmbed()
+            .setThumbnail(getImageUrlFromName(skinName))
+            .setDescription(`Skin Status of ${skinName}`)
+            .setColor("#d45f93")
+            .addField(
+                `Trade status: ${
+                    data.type
+                } \nUse ${prefix}notify ${skinName} to get notified when the skin is tradeable`,
+                `[Get ${skinName} on CS.Money](https://cs.money/?s=float#skin_name_buy=${encodeURIComponent(
+                    skinName
+                )}${skinName.endsWith(")") ? "" : ")"}`
+            );
+        return embedResponse;
+    } else {
+        //let botAvatar = bot.user.displayAvatarURL;
+        let embedResponse = new Discord.RichEmbed()
+            .setThumbnail(getImageUrlFromName(skinName))
+            .setDescription(`Skin Status of ${skinName}`)
+            .setColor("#d45f93")
+            .addField(
+                `Trade status: ${data.type} \nAmount till overstock: ${
+                    data.overstock_difference
+                }`,
+                `[Get ${skinName} on CS.Money](https://cs.money/?s=float#skin_name_buy=${encodeURIComponent(
+                    skinName
+                )}${skinName.endsWith(")") ? "" : ")"}`
+            );
+        return embedResponse;
+    }
+}
+
+function checkItemStatus2(docs) {
+    if (docs.length > 0) {
+        currentDoc = docs[0];
+        got(
+            `https://cs.money/check_skin_status?market_hash_name=${
+                currentDoc.skin
+            }&appid=730`,
+            {json: true}
+        )
+            .then(response => {
+                let data = response.body;
+                if (data.type == "Tradable") {
+                    console.log(currentDoc);
+                    var userPromises = [];
+                    for (var idIndex in currentDoc.ids) {
+                        userPromises.push(
+                            bot
+                                .fetchUser(currentDoc.ids[idIndex])
+                                .then(user => {
+                                    console.log(user.id + currentDoc.skin);
+                                    user.send(
+                                        getRichEmbed(data, currentDoc.skin)
+                                    );
+                                })
+                        );
+                    }
+                    Promise.all(userPromises).then(function() {
+                        db.remove({_id: currentDoc._id}, {}, function(
+                            err,
+                            numRemoved
+                        ) {});
+                        checkItemStatus2(docs.slice(1));
+                    });
+                } else {
+                    checkItemStatus2(docs.slice(1));
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+}
+
+function startItemCheck() {
+    db.find({}, function(err, docs) {
+        checkItemStatus2(docs);
+    });
+    setTimeout(function() {
+        startItemCheck();
+    }, 10 * 60 * 1000);
+}
+
+function checkItemStatus() {
+    db.find({}, function(err, docs) {
+        for (var docIndex in docs) {
+            got(
+                `https://cs.money/check_skin_status?market_hash_name=${
+                    docs[docIndex].skin
+                }&appid=730`,
+                {json: true}
+            )
+                .then(response => {
+                    let data = response.body;
+                    if (data.type == "Tradable") {
+                        for (var idIndex in docs[docIndex].ids) {
+                            bot.fetchUser(docs[docIndex].ids[idIndex]).then(
+                                user =>
+                                    user.send(
+                                        getRichEmbed(data, docs[docIndex].skin)
+                                    )
+                            );
+                        }
+                        db.remove({_id: docs[docIndex]._id}, {}, function(
+                            err,
+                            numRemoved
+                        ) {});
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+    });
+}
+
+// module.exports = {
+//     similarity,
+//     editDistance,
+//     getImageUrlFromName,
+//     getRichEmbed,
+//     checkItemStatus2,
+//     startItemCheck,
+//     checkItemStatus
+//};
+module.exports = function(_bot) {
+    bot = _bot;
+    return {
+        similarity,
+        editDistance,
+        getImageUrlFromName,
+        getRichEmbed,
+        checkItemStatus2,
+        startItemCheck,
+        checkItemStatus
+    };
+};
--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const got = require("got");
+
+const bot = new Discord.Client({disableEveryone: true});
+
+const exf = require("./external_functions")(bot);
 var fs = require("fs");
 const store = require("nedb");
 const db = new store({filename: "database.db", autoload: true});
 
-const bot = new Discord.Client({disableEveryone: true});
 const prefix = botconfig.prefix;
 
 bot.on("ready", async () => {
+    let prefix = botconfig.prefix;
     console.log(`${bot.user.username} online`);
-    bot.user.setActivity("Here to help you!");
+    bot.user.setActivity(`${prefix}help`);
     console.log("Starting ItemCheck");
-    startItemCheck();
+    exf.startItemCheck();
 });
 
 bot.on("message", async message => {
@@ -48,11 +52,26 @@ bot.on("message", async message => {
         return message.channel.send(embedMessage);
     }
 
+    if (cmd === `${prefix}info`) {
+        let botAvatar = bot.user.displayAvatarURL;
+        let botembed = new Discord.RichEmbed()
+            .setDescription("Bot usage Information")
+            .setColor("#d45f93")
+            .setThumbnail(botAvatar)
+            .addField(
+                "Hello, my name is " +
+                    bot.user.username +
+                    ", I am here to help you, please be aware that I am still learning to do stuff ;)",
+                "CS.MONEY Developer Team"
+            );
+
+        return message.channel.send(botembed);
+    }
+
     if (cmd === `${prefix}report`) {
         let userReporterId = message.author.id;
         let userMessage = message.content.match(/cs.report(.*)/); // Не забывать менять префикс
         let messageId = message.id;
-        let messageTime = message.createdTimestamp;
         let embedMessage = new Discord.RichEmbed()
             .setDescription("Wrong parametres!")
             .addField(
@@ -134,7 +153,7 @@ bot.on("message", async message => {
                 let highestIndex = 0;
                 let highestRate = 0;
                 for (var i = 0; i < data.length; i++) {
-                    let similarityscore = similarity(skinName, data[i]);
+                    let similarityscore = exf.similarity(skinName, data[i]);
                     if (similarityscore > highestRate) {
                         highestRate = similarityscore;
                         highestIndex = i;
@@ -149,7 +168,7 @@ bot.on("message", async message => {
                 ).then(response => {
                     let data = response.body;
 
-                    let embedResponse = getRichEmbed(data, skinName);
+                    let embedResponse = exf.getRichEmbed(data, skinName);
 
                     message.reply({
                         embed: embedResponse
@@ -166,14 +185,14 @@ bot.on("message", async message => {
                 .then(response => {
                     let data = response.body;
 
-                    let embedResponse = getRichEmbed(data, skinName);
+                    let embedResponse = exf.getRichEmbed(data, skinName);
 
                     message.reply({
                         embed: embedResponse
                     });
                 })
                 .catch(error => {
-                    return message.reply("skin not found");
+                    return message.reply("Skin not found");
                 });
         }
     }
@@ -196,7 +215,7 @@ bot.on("message", async message => {
             let highestIndex = 0;
             let highestRate = 0;
             for (var i = 0; i < data.length; i++) {
-                let similarityscore = similarity(skinName, data[i]);
+                let similarityscore = exf.similarity(skinName, data[i]);
                 if (similarityscore > highestRate) {
                     highestRate = similarityscore;
                     highestIndex = i;
@@ -245,162 +264,40 @@ bot.on("message", async message => {
             files: ["./support_placement.png"]
         });
     }
-});
 
-function startItemCheck() {
-    db.find({}, function(err, docs) {
-        checkItemStatus2(docs);
-    });
-    setTimeout(function() {
-        startItemCheck();
-    }, 10 * 60 * 1000);
-}
-
-function checkItemStatus() {
-    db.find({}, function(err, docs) {
-        for (var docIndex in docs) {
-            got(
-                `https://cs.money/check_skin_status?market_hash_name=${
-                    docs[docIndex].skin
-                }&appid=730`,
-                {json: true}
+    if (cmd === `${prefix}shop`) {
+        if (
+            !message.member.roles.find(
+                "name",
+                "Trader" || "Senior Trader" || "Contributor" || "CS.Money"
             )
-                .then(response => {
-                    let data = response.body;
-                    if (data.type == "Tradable") {
-                        for (var idIndex in docs[docIndex].ids) {
-                            bot.fetchUser(docs[docIndex].ids[idIndex]).then(
-                                user =>
-                                    user.send(
-                                        getRichEmbed(data, docs[docIndex].skin)
-                                    )
-                            );
-                        }
-                        db.remove({_id: docs[docIndex]._id}, {}, function(
-                            err,
-                            numRemoved
-                        ) {});
-                    }
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-        }
-    });
-}
-
-function checkItemStatus2(docs) {
-    if (docs.length > 0) {
-        currentDoc = docs[0];
-        got(
-            `https://cs.money/check_skin_status?market_hash_name=${
-                currentDoc.skin
-            }&appid=730`,
-            {json: true}
         )
-            .then(response => {
-                let data = response.body;
-                if (data.type == "Tradable") {
-                    for (var idIndex in currentDoc.ids) {
-                        bot.fetchUser(currentDoc.ids[idIndex]).then(user =>
-                            user.send(getRichEmbed(data, currentDoc.skin))
-                        );
-                    }
-                    db.remove({_id: currentDoc._id}, {}, function(
-                        err,
-                        numRemoved
-                    ) {});
-                }
-                checkItemStatus2(docs.slice(1));
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }
-}
-
-function getRichEmbed(data, skinName) {
-    if (data.overstock_difference == undefined) {
-        let botAvatar = bot.user.displayAvatarURL;
-
-        let embedResponse = new Discord.RichEmbed()
-            .setThumbnail(getImageUrlFromName(skinName))
-            .setDescription(`Skin Status of ${skinName}`)
-            .setColor("#d45f93")
-            .addField(
-                `Trade status: ${
-                    data.type
-                } \nUse ${prefix}notify ${skinName} to get notified when the skin is tradeable`,
-                `[Get ${skinName} on CS.Money](https://cs.money/?s=float#skin_name_buy=${encodeURIComponent(
-                    skinName
-                )}${skinName.endsWith(")") ? "" : ")"}`
+            return message.reply("You are not allowed to use this command ;)");
+        let messageAuthor = message.author.id;
+        let authorAvatar = message.author.displayAvatarURL;
+        let attachmentArray = message.attachments.array()[0];
+        let shopLink = message.content
+            .split(" ")
+            .slice(1)
+            .join(" ");
+        console.log("link", shopLink);
+        if (!shopLink)
+            return message.reply("You did not specify your sellerid.");
+        if (!attachmentArray)
+            return message.reply("You did not specify your shop image.");
+        let embedShop = new Discord.RichEmbed()
+            .setDescription(`<@!${messageAuthor}> CS.Money shop`)
+            .addField("Link to the shop:", `${shopLink}`)
+            .setThumbnail(`${authorAvatar}`)
+            .setImage(attachmentArray.url);
+        console.log(attachmentArray, shopLink);
+        if (!shopLink.match(/https?\:\/\/([\w\d\.]+)?cs\.money\/#sellerid=\d+/))
+            return message.reply(
+                "Please use correct shop link, for example https://cs.money/#sellerid=YOUR_ID"
             );
-        return embedResponse;
-    } else {
-        let botAvatar = bot.user.displayAvatarURL;
-        let embedResponse = new Discord.RichEmbed()
-            .setThumbnail(getImageUrlFromName(skinName))
-            .setDescription(`Skin Status of ${skinName}`)
-            .setColor("#d45f93")
-            .addField(
-                `Trade status: ${data.type} \nAmount till overstock: ${
-                    data.overstock_difference
-                }`,
-                `[Get ${skinName} on CS.Money](https://cs.money/?s=float#skin_name_buy=${encodeURIComponent(
-                    skinName
-                )}${skinName.endsWith(")") ? "" : ")"}`
-            );
-        return embedResponse;
+        bot.channels.find("name", "sellerid-showcase").send(embedShop);
+        message.delete(200);
     }
-}
-
-function getImageUrlFromName(skinName) {
-    //Probably better to store in memory
-    var skinList = JSON.parse(fs.readFileSync("skinList.json", "utf8"));
-    return `https://pic.csgo.trade/730/${skinList[skinName]}.jpg?v=22 `;
-}
-
-function similarity(s1, s2) {
-    var longer = s1;
-    var shorter = s2;
-    if (s1.length < s2.length) {
-        longer = s2;
-        shorter = s1;
-    }
-    var longerLength = longer.length;
-    if (longerLength == 0) {
-        return 1.0;
-    }
-    return (
-        (longerLength - editDistance(longer, shorter)) /
-        parseFloat(longerLength)
-    );
-}
-
-function editDistance(s1, s2) {
-    s1 = s1.toLowerCase();
-    s2 = s2.toLowerCase();
-
-    var costs = new Array();
-    for (var i = 0; i <= s1.length; i++) {
-        var lastValue = i;
-        for (var j = 0; j <= s2.length; j++) {
-            if (i == 0) costs[j] = j;
-            else {
-                if (j > 0) {
-                    var newValue = costs[j - 1];
-                    if (s1.charAt(i - 1) != s2.charAt(j - 1))
-                        newValue =
-                            Math.min(Math.min(newValue, lastValue), costs[j]) +
-                            1;
-                    costs[j - 1] = lastValue;
-                    lastValue = newValue;
-                }
-            }
-        }
-        if (i > 0) costs[s2.length] = lastValue;
-    }
-    return costs[s2.length];
-}
+});
 
 bot.login(botconfig.token);
