1b5e905dc5c7d92c9a749f569be7b89bdd6a43d3
babkin_nikita <shrud2k@gmail.com>
2019-06-11 12:56:56 +0300

    fixed notify

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const got = require("got");
-const exf = require("./external_functions");
+
+const bot = new Discord.Client({ disableEveryone: true });
+
+const exf = require("./external_functions")(bot);
 var fs = require("fs");
 const store = require("nedb");
 const db = new store({ filename: "database.db", autoload: true });
 
-const bot = new Discord.Client({ disableEveryone: true });
 const prefix = botconfig.prefix;
 
 bot.on("ready", async () => {
