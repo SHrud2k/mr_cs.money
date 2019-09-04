e3f154fe7657142eca3f48bc9d7076368b2564dd
SHrud2k <36342977+SHrud2k@users.noreply.github.com>
2019-09-04 15:26:34 +0300

    fix conf

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const got = require("got");
-const bot = new Discord.Client({ disableEveryone: true });
+const bot = new Discord.Client({disableEveryone: true});
 var fs = require("fs");
 const store = require("nedb");
 const puppeteer = require("puppeteer");
-const db = new store({ filename: "./database.db", autoload: true });
+const db = new store({filename: "./database.db", autoload: true});
 
 const exf = require("./external_functions")(bot, db);
 const prefix = botconfig.prefix;
@@ -53,6 +53,10 @@ bot.on("message", async message => {
                 `${prefix}notify`,
                 "Want to be notified when your favourite skin will get out of overstock? You can by executing this command."
             )
+            .addField(
+                `${prefix}online`,
+                "Check the status of the website services"
+            )
             .setColor("#d45f93")
             .setThumbnail(botAvatar);
         return message.channel.send(embedMessage);
@@ -82,7 +86,7 @@ bot.on("message", async message => {
             .setDescription("Wrong parametres!")
             .addField(
                 "Please try again, with this example:",
-                "csm.report {USER_TO_REPORT} {REASON_MESSAGE}"
+                "cs.report {USER_TO_REPORT} {REASON_MESSAGE}"
             );
         if (userMessage) {
             const content = userMessage[1].trim().replace(/\s{2,}/g, " ");
@@ -161,7 +165,7 @@ bot.on("message", async message => {
                 `https://cs.money/get_auto_complete?part_name=${encodeURIComponent(
                     skinName
                 )}&appid=730`,
-                { json: true }
+                {json: true}
             ).then(response => {
                 let data = response.body;
                 if (data.length == 0) {
@@ -181,7 +185,7 @@ bot.on("message", async message => {
                     `https://cs.money/check_skin_status?market_hash_name=${encodeURIComponent(
                         skinName
                     )}&appid=730`,
-                    { json: true }
+                    {json: true}
                 ).then(response => {
                     let data = response.body;
 
@@ -197,7 +201,7 @@ bot.on("message", async message => {
                 `https://cs.money/check_skin_status?market_hash_name=${encodeURIComponent(
                     skinName
                 )}&appid=730`,
-                { json: true }
+                {json: true}
             )
                 .then(response => {
                     let data = response.body;
@@ -223,7 +227,7 @@ bot.on("message", async message => {
             `https://cs.money/get_auto_complete?part_name=${encodeURIComponent(
                 skinName
             )}&appid=730`,
-            { json: true }
+            {json: true}
         ).then(response => {
             let data = response.body;
             if (data.length == 0) {
@@ -239,7 +243,7 @@ bot.on("message", async message => {
                 }
             }
             skinName = data[highestIndex];
-            db.findOne({ skin: skinName }, function(err, data) {
+            db.findOne({skin: skinName}, function(err, data) {
                 if (data == null) {
                     data = {
                         skin: skinName,
@@ -250,8 +254,8 @@ bot.on("message", async message => {
                     if (!data.ids.includes(authorid)) {
                         data.ids.push(authorid);
                         db.update(
-                            { skin: skinName },
-                            { skin: skinName, ids: data.ids },
+                            {skin: skinName},
+                            {skin: skinName, ids: data.ids},
                             {},
                             function(err, numReplaced) {}
                         );
@@ -282,6 +286,142 @@ bot.on("message", async message => {
         });
     }
 
+    if (cmd === `${prefix}online`) {
+        got(`https://cs.money/`)
+            .then(response => {
+                let data = response.body;
+                var ex = new RegExp('"work_statuses":{([^}]+)}');
+                var exres = data
+                    .match(ex)[0]
+                    .replace('"work_statuses":', "[")
+                    .concat("]");
+                var json = JSON.parse(exres)[0];
+
+                const payment = [
+                    "disable_g2a",
+                    "disable_paymaster",
+                    "disable_qiwi",
+                    "disable_walletone",
+                    "disable_tinkoff"
+                ];
+                var paymentcount = 5;
+                for (var i = 0; i < payment.length; i++) {
+                    if (json[payment[i]]) {
+                        paymentcount--;
+                    }
+                }
+                const trading = [
+                    "disable_deposit",
+                    "disable_withdraw",
+                    "disable_sell"
+                ];
+                var tradingcount = 3;
+                for (var i = 0; i < trading.length; i++) {
+                    if (json[trading[i]]) {
+                        tradingcount--;
+                    }
+                }
+                let misc = ["fuckup", "steam_problems"];
+                var misccount = 2;
+                for (var i = 0; i < misc.length; i++) {
+                    if (json[misc[i]]) {
+                        misccount--;
+                    }
+                }
+                let embedResponse = new Discord.RichEmbed()
+                    .setColor("#d45f93")
+                    .setThumbnail(bot.user.displayAvatarURL)
+                    .setTitle("CS.Money service overview")
+                    .addField(
+                        "Maintenance",
+                        json["maintenance"]
+                            ? `Currently under maintenance`
+                            : `Currently not under maintenance`
+                    )
+                    .addField(
+                        "Payment (" + paymentcount + "/5)",
+                        paymentcount == 5
+                            ? `All payment services are running ✔️`
+                            : `Not all services are running ❌`
+                    )
+                    .addField(
+                        "Trading (" + tradingcount + "/3)",
+                        tradingcount == 3
+                            ? `All trading services are running ✔️`
+                            : `Not all services are running ❌`
+                    )
+                    .addField(
+                        "Other (" + misccount + "/2)",
+                        misccount == 2
+                            ? `All other services are running ✔️`
+                            : `Not all services are running ❌`
+                    );
+                message.reply(embedResponse);
+            })
+            .catch(error => {
+                console.log(error);
+                message.reply("Error while getting Website status");
+            });
+    }
+
+    if (cmd === `${prefix}stats`) {
+        got(`https://cs.money/`)
+            .then(response => {
+                let data = response.body;
+                //[{"new_users_per_day":6224,"total_count_offers":67681385,"total_count_users":4695918,"user_online":"4289","array_offers_stats":[67681299,67681306,67681312,67681314,67681319,67681327,67681348,67681357,67681372,67681379,67681385]}]
+                var ex = new RegExp('"list_stats":{([^}]+)}');
+                var exres = data
+                    .match(ex)[0]
+                    .replace('"list_stats":', "[")
+                    .concat("]");
+                var json = JSON.parse(exres)[0];
+
+                let embedResponse = new Discord.RichEmbed()
+                    .setColor("#d45f93")
+                    .setThumbnail(bot.user.displayAvatarURL)
+                    .setTitle("CS.Money stats overview")
+                    .addField(
+                        "User statistics",
+                        `Online users: ${json[`user_online`]}\nUnique users: ${
+                            json[`total_count_users`]
+                        }\nNew users today: ${json[`new_users_per_day`]}`
+                    )
+                    .addField(
+                        "Trade statistics",
+                        `Succesfull trades: ${
+                            json[`total_count_offers`]
+                        }\nTrades in the last 5 seconds: ${json[
+                            `array_offers_stats`
+                        ][10] - json[`array_offers_stats`][9]}`
+                    );
+
+                /*.addField(json[`user_online`], "ONLINE USERS", true)
+                    .addField(
+                        json[`array_offers_stats`][10] -
+                            json[`array_offers_stats`][9],
+                        "TRADES IN THE LAST 5 SECONDS",
+                        true
+                    )
+                    .addField(json[`total_count_users`], "LOYAL USERS", true)
+                    .addField(
+                        json[`total_count_offers`],
+                        "SUCCESFUL TRADES",
+                        true
+                    )
+                    .addField(
+                        json[`new_users_per_day`],
+                        "NEW USERS TODAY",
+                        true
+                    );*/
+
+                message.reply(embedResponse);
+            })
+            .catch(error => {
+                console.log(error);
+                message.reply("Error while getting Website status");
+            });
+    }
+
     if (cmd === `${prefix}shop`) {
         if (
             !message.member.roles.some(role => {
@@ -310,15 +450,12 @@ bot.on("message", async message => {
         (async () => {
             const browser = await puppeteer.launch();
             const page = await browser.newPage();
-            page.setViewport({
-                width: 1900,
-                height: 1080,
-                deviceScaleFactor: 2
-            });
+            page.setViewport({width: 4000, height: 1080, deviceScaleFactor: 2});
 
             await page.goto(shopLink, {
                 waitUntil: "networkidle2"
             });
+
             await page.waitForSelector(
                 "#main_container_bot[state=filled]" ||
                     "#main_container_user[state=filled]"
@@ -333,7 +470,7 @@ bot.on("message", async message => {
                         width,
                         height
                     } = element.getBoundingClientRect();
-                    return { left: x, top: y, width, height, id: element.id };
+                    return {left: x, top: y, width, height, id: element.id};
                 }, selector);
 
                 return await page.screenshot({
@@ -344,24 +481,28 @@ bot.on("message", async message => {
                         x: rect.left - padding,
                         y: rect.top - padding,
                         width: rect.width + padding * 2,
-                        height: rect.height + padding * 2
+                        height: Math.min(rect.height + padding * 2, 585)
                     }
                 });
             }
 
-            await screenshotDOMElement("#main_container_bot", 16);
-            const attachment = new Discord.Attachment(
-                `${sellerScreenshotsDir}/${shopLink.split("=")[1]}.png`,
-                `${shopLink.split("=")[1]}.png`
-            );
-            let embedShop = new Discord.RichEmbed()
-                .setDescription(`<@!${messageAuthor}> CS.Money shop`)
-                .addField("Link to the shop:", `${shopLink}`)
-                .setThumbnail(`${authorAvatar}`)
-                .attachFile(attachment)
-                .setImage(`attachment://${shopLink.split("=")[1]}.png`);
-            bot.channels.find("name", "🔄sellerid-showcase").send(embedShop);
-            message.delete(200);
+            setTimeout(async function() {
+                await screenshotDOMElement("#main_container_bot", 16);
+                const attachment = new Discord.Attachment(
+                    `${sellerScreenshotsDir}/${shopLink.split("=")[1]}.png`,
+                    `${shopLink.split("=")[1]}.png`
+                );
+                let embedShop = new Discord.RichEmbed()
+                    .setDescription(`<@!${messageAuthor}> CS.Money shop`)
+                    .addField("Link to the shop:", `${shopLink}`)
+                    .setThumbnail(`${authorAvatar}`)
+                    .attachFile(attachment)
+                    .setImage(`attachment://${shopLink.split("=")[1]}.png`);
+                bot.channels
+                    .find("name", "🔄sellerid-showcase")
+                    .send(embedShop);
+                message.delete(200);
+            }, 1000 * 2);
         })();
     }
 });
